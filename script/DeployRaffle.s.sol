dca93391fdcc6c296ee33a015e0c9e97b0556b6b
Mavdavis <ekwesimarvelous@gmail.com>
2025-07-29 06:36:00 +0100

    wip

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import Raffle from "src/Raffle.sol";
+import {Raffle} from "../src/Raffle.sol";
 
 contract DeployRaffle is Script{
     function run()public{
